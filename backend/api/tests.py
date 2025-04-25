ac1ec498c28c6f95c7572a6fc60535fe789d12b8
Dablek <dableshevich.daniil@gmail.com>
2025-04-25 17:23:27 +0300

    Update workflow for tests 2

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -6,16 +6,16 @@ from django.test import Client, TestCase
 
 class TaskiAPITestCase(TestCase):
     def setUp(self):
-	self.guest_client = Client()
+        self.guest_client = Client()
 
     def test_list_exists(self):
-	"""Проверка доступности списка задач."""
-	response = self.guest_client.get('/api/tasks/')
-	self.assertEqual(response.status_code, HTTPStatus.OK)
+        """Проверка доступности списка задач."""
+        response = self.guest_client.get('/api/tasks/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_task_creation(self):
-	"""Проверка создания задачи."""
-	data = {'title': 'Test', 'description': 'Test'}
-	response = self.guest_client.post('/api/tasks/', data=data)
-	self.assertEqual(response.status_code, HTTPStatus.CREATED)
-	self.assertTrue(models.Task.objects.filter(title='Test').exists())
+        """Проверка создания задачи."""
+        data = {'title': 'Test', 'description': 'Test'}
+        response = self.guest_client.post('/api/tasks/', data=data)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertTrue(models.Task.objects.filter(title='Test').exists())
